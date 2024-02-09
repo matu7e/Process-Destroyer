48706e6f6882362755093f9c530f7e99770204d7
matu7e <101483347+matu7e@users.noreply.github.com>
2024-02-09 18:24:55 -0300

    Update destructor_de_procesos.sh

--- destructor_de_procesos.sh
+++ destructor_de_procesos.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-read -p "ingrese el nombre de un proceso a eliminar: " variable
+read -p "Ingrese el nombre de un proceso a eliminar: " ProcesoCorriendo
 
 
 
 
 for ((i=1; ; i++))
 do
-	vueltas=$(ps -el | grep $variable | tr -s " " " " | cut -d " " -f 4 | wc -l )
+	vueltas=$(ps -el | grep $ProcesoCorriendo | tr -s " " " " | cut -d " " -f 4 | wc -l )
 	if [[ "$vueltas" -ne 0 ]]
 	
 		
 		then 	
-			pid=$(ps -el | grep $variable | tr -s " " " " | cut -d " " -f 4 | head -n 1 )
+			pid=$(ps -el | grep $ProcesoCorriendo | tr -s " " " " | cut -d " " -f 4 | head -n 1 )
 			kill -9 $pid
 			
 		
 		
 		else			
 			cont=$((i - 1))
-			echo "se eliminaron $cont procesos llamados $variable"
+			echo "se eliminaron $cont procesos llamados $ProcesoCorriendo"
 			break
 			
 	fi
